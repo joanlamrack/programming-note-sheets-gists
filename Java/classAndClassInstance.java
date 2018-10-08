463999d69ef5f97a93c55c73faed0f486b4e7978
Joan Lamrack <joanlamrack@gmail.com>
2018-10-08 17:36:58 +0700

    added class variables and calling instance variables

--- Java/classAndClassInstance.java
+++ Java/classAndClassInstance.java
@@ -1,10 +1,27 @@
 public class classAndClassInstance {
+	String stringContainer;
+	String name;
+
 	public classAndClassInstance(String name) {
 		System.out.println(name);
+		this.name = name;
+	}
+
+	public void setStringContainer(String input) {
+		stringContainer = input;
+	}
+
+	public String getString() {
+		return stringContainer;
 	}
 
 	public static void main(String[] args) {
 		classAndClassInstance newInstance = new classAndClassInstance("hehe");
+
+		newInstance.setStringContainer("Haha");
+
+		System.out.println("the name is " + newInstance.name + "\n" + "String in the container is: "
+				+ newInstance.stringContainer);
 	}
 }
 
