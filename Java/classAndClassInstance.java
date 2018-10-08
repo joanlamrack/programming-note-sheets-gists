13b8f5095396b18dd042a2dae535418833c74e9a
Joan Lamrack <joanlamrack@gmail.com>
2018-10-08 17:22:38 +0700

    Added class

---
+++ Java/classAndClassInstance.java
@@ -0,0 +1,21 @@
+public class classAndClassInstance {
+	public classAndClassInstance(String name) {
+		System.out.println(name);
+	}
+
+	public static void main(String[] args) {
+		classAndClassInstance newInstance = new classAndClassInstance("hehe");
+	}
+}
+
+/*
+ * Class and instance.
+ * 
+ * public class <ClassName>{ //constructor public <ClassName>(<Any Parameter>){
+ * 
+ * }
+ * 
+ * public <return type> <methodName> (<Method Params>){
+ * 
+ * } }
+ */
