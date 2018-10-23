72885d383df9fc244572931b76c628e83793dd16
Joan Lamrack <joanlamrack@gmail.com>
2018-10-23 10:08:32 +0700

    add mode looker

---
+++ Java/Exercise/4/lookingformodus.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class lookingformodus{
+	public static void main(String[]args){
+		HashMap<Integer, Integer> input = dolookingformodus(new int[]{1,3,3});
+
+		System.out.println(Arrays.asList(input));
+	}
+
+	public static HashMap<Integer, Integer> dolookingformodus(int[] input){
+		HashMap<Integer,Integer> container= new HashMap<Integer,Integer>();
+		int biggestModeNumber = 0;
+		int occurences = 0;
+
+		for(Integer number : input){
+			Integer container_content = container.get(number);
+
+			container.put(number, container_content != null ? container_content + 1 :  1);
+		}
+
+		for(Integer key : container.keySet() ){
+			if(biggestModeNumber == 0 || occurences < container.get(key)){
+				biggestModeNumber = key;
+				occurences = container.get(key);
+			}
+
+		}
+		
+		return biggestModeNumber;
+	}
+}
