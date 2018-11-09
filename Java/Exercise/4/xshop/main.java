bbae8ca90a21cfc4985050e56bfe57a7a5124afd
Joan Lamrack <joanlamrack@gmail.com>
2018-11-09 08:55:26 +0700

    add main logic

--- Java/Exercise/4/xshop/main.java
+++ Java/Exercise/4/xshop/main.java
@@ -1,5 +1,8 @@
 public class main{
 	public static void main(String[]args){
-		
+		Shopper[] shoppers = new Shopper[]{new Shopper("Dina", "Sepatu Stacattu", 2), new Shopper("Dina2", "Sepatu Stacattu", 2)};
+
+		Xshop.assessShoppers(shoppers);
+		Xshop.printShopContent();
 	}
 }
