41860714850107d66fc025d523af5086288af0e3
Joan Lamrack <joanlamrack@gmail.com>
2018-11-10 22:29:04 +0700

    passwordGen initial setup

---
+++ Java/Exercise/4/passwordGenerator.java
@@ -0,0 +1,25 @@
+public class passwordGenerator{
+	public static void main(String[]args){
+		
+	}
+
+	public static String changeVocals(String input){
+		return "";
+	}
+
+	public static String reverseWord(String input){
+		return "";
+	}
+
+	public static String setLowerUpperCase(String input){
+		return "";
+	}
+
+	public static String removeSpaces(String input){
+		return "";
+	}
+
+	public static String dopasswordGenerator(String input){
+		return "";
+	}
+}
