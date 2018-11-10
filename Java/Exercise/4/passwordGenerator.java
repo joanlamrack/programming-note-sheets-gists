01c5d1aab70a3f933e5a91c0341408fd3fbc2b00
Joan Lamrack <joanlamrack@gmail.com>
2018-11-10 23:20:17 +0700

    set lower upper case

--- Java/Exercise/4/passwordGenerator.java
+++ Java/Exercise/4/passwordGenerator.java
@@ -1,6 +1,9 @@
 public class passwordGenerator{
 	public static void main(String[]args){
-		
+		String oi = "hey heC";
+
+		System.out.println(setLowerUpperCase(oi));
+
 	}
 
 	public static String changeVocals(String input){
@@ -13,11 +16,22 @@ public class passwordGenerator{
 	}
 
 	public static String setLowerUpperCase(String input){
-		return "";
+		char[] output = input.toCharArray();
+
+		for(int start = 0 ; start < output.length; start++){
+			if(Character.isLowerCase(output[start])){
+				output[start] = Character.toUpperCase(output[start]);
+			}
+			else if(Character.isUpperCase(output[start])){
+				output[start] = Character.toLowerCase(output[start]);
+			}
+		}
+
+		return new String(output);
 	}
 
 	public static String removeSpaces(String input){
-		return "";
+		return input.replaceAll("\\s", "");
 	}
 
 	public static String dopasswordGenerator(String input){
