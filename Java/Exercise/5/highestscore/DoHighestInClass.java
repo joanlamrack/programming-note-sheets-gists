ff0c4211c7eb6397dd98a670008fcbbe07dd1c87
Joan Lamrack <joanlamrack@gmail.com>
2018-11-21 08:05:07 +0700

    added doHighest in Class

---
+++ Java/Exercise/5/highestscore/DoHighestInClass.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DoHighestInClass{
+	private static HashMap<String,Class> classes = new ArrayList<Class>();
+	public static assessStudents(Student[] students){
+		for(Student oneStudent : students){
+			assessStudent(oneStudent);
+		}
+	}
+
+	public static assessStudent(Student oneStudent){
+		Class theClassWithSuchName = classes.get(className);
+		String className = oneStudent.getClassName();
+		int currentStudentScore = oneStudent.getScore();
+		
+		if(theClassWithSuchName == null){
+			classes.set(className, new Class(className, oneStudent));
+		}
+		else if(theClassWithSuchName.getBestStudentScore() < currentStudentScore){
+			theClassWithSuchName.setBestStudent(oneStudent);
+		}
+	}
+
+	@Override
+	public String toString() {
+		String output = "";
+		for(String key : classes.keySet()){
+			output += classes.get(key).toString();
+		}
+
+		return output;
+	}
+
+}
