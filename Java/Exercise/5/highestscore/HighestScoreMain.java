74ad2a16edb582ca9855ed787fd799050e9cc7d3
Joan Lamrack <joanlamrack@gmail.com>
2018-11-21 08:29:39 +0700

    added main program

--- Java/Exercise/5/highestscore/HighestScoreMain.java
+++ Java/Exercise/5/highestscore/HighestScoreMain.java
@@ -1,5 +1,11 @@
 public class HighestScoreMain{
 	public static void main(String[]args){
-		
+		Student[] students = {
+			new Student("foxes", "Alexander", 100),
+			new  Student("wolves", "Alisa", 76),
+			new Student("foxes", "Vladimir", 92)
+		};
+
+		DoHighestInClass.assessStudents(students);
 	}
 }
