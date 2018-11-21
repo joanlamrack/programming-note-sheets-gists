fc6a96fba538613815b859239c1b65ac02564e8a
Joan Lamrack <joanlamrack@gmail.com>
2018-11-21 08:33:53 +0700

    change the references

--- Java/Exercise/5/highestscore/DoHighestInClass.java
+++ Java/Exercise/5/highestscore/DoHighestInClass.java
@@ -1,32 +1,32 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class DoHighestInClass{
-	private static HashMap<String,Class> classes = new HashMap<String,Class>();
-	public static void assessStudents(Student[] students){
-		for(Student oneStudent : students){
+public class DoHighestInClass {
+	private static HashMap<String, StudentClass> classes = new HashMap<String, StudentClass>();
+
+	public static void assessStudents(Student[] students) {
+		for (Student oneStudent : students) {
 			assessStudent(oneStudent);
 		}
 	}
 
-	public static void assessStudent(Student oneStudent){
-		
+	public static void assessStudent(Student oneStudent) {
+
 		String className = oneStudent.getClassName();
-		Class theClassWithSuchName = classes.get(className);
+		StudentClass theClassWithSuchName = classes.get(className);
 		int currentStudentScore = oneStudent.getScore();
-		
-		if(theClassWithSuchName == null){
-			classes.put(className, new Class(className, oneStudent));
-		}
-		else if(theClassWithSuchName.getBestStudentScore() < currentStudentScore){
+
+		if (theClassWithSuchName == null) {
+			classes.put(className, new StudentClass(className, oneStudent));
+		} else if (theClassWithSuchName.getBestStudentScore() < currentStudentScore) {
 			theClassWithSuchName.setBestStudent(oneStudent);
 		}
 	}
 
 	public static String getContent() {
 		String output = "";
-		for(String key : classes.keySet()){
-			output += classes.get(key).toString();
+		for (String key : classes.keySet()) {
+			output += classes.get(key).toString()+"\n";
 		}
 
 		return output;
--- Java/Exercise/5/highestscore/HighestScoreMain.java
+++ Java/Exercise/5/highestscore/HighestScoreMain.java
@@ -1,11 +1,13 @@
 public class HighestScoreMain{
 	public static void main(String[]args){
 		Student[] students = {
+			new Student("foxes", "Vladimir", 92),
 			new Student("foxes", "Alexander", 100),
-			new  Student("wolves", "Alisa", 76),
-			new Student("foxes", "Vladimir", 92)
+			new Student("wolves", "Alisa", 76)
+			
 		};
 
 		DoHighestInClass.assessStudents(students);
+		System.out.println(DoHighestInClass.getContent());
 	}
 }
