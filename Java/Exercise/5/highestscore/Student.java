0c09f504a16b526fed443267557790d2b9572813
Joan Lamrack <joanlamrack@gmail.com>
2018-11-21 07:13:52 +0700

    added studen class

---
+++ Java/Exercise/5/highestscore/Student.java
@@ -0,0 +1,23 @@
+public class Student{
+	private String belongToClass = "";
+	private String name = "";
+	private int score = "";
+	
+	public Student(String className, String studentName, int studentScore){
+		this.belongToClass = className;
+		this.name = studentName;
+		this.score = studentScore;
+	}
+
+	public String getClassName(){
+		return this.belongToClass;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public int getScore(){
+		return this.score;
+	}
+}
