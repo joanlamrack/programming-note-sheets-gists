71d7b51a3ccb4c40689d082537e0006d138b7d90
Joan Lamrack <joanlamrack@gmail.com>
2018-11-22 08:36:08 +0700

    added student

---
+++ Java/Exercise/5/graduates/Student.java
@@ -0,0 +1,23 @@
+public class Student{
+	private String name = "";
+	private String className = "";
+	private int score = 0;
+
+	public Student(String studentName, String className, String studentScore){
+		this.name = studentName;
+		this.className = className;
+		this.score = studentScore;
+	}
+
+	public String getStudentName(){
+		return this.name;
+	}
+
+	public String getClassName(){
+		return this.className;
+	}
+
+	public int getStudentScore(){
+		return this.score;
+	}
+}
