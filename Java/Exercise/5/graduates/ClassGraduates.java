573628dd5431d590c8e101993a14ac418623ca8e
Joan Lamrack <joanlamrack@gmail.com>
2018-11-22 09:31:52 +0700

    added skbm

--- Java/Exercise/5/graduates/ClassGraduates.java
+++ Java/Exercise/5/graduates/ClassGraduates.java
@@ -2,13 +2,16 @@ import java.util.ArrayList;
 
 public class ClassGraduates{
 	private ArrayList<Student> studentsGraduated = new ArrayList<Student>();
+	private int skbm = 75;
 
 	public ClassGraduates(Student firstStudent){
-		this.studentsGraduated.add(firstStudent);
+		this.addMoreStudent(firstStudent);
 	}
 
-	public void addMoreStudent(Student additionalStudent){
-		this.studentsGraduated.add(additionalStudent);
+	public void addMoreStudentGraduated(Student additionalStudent){
+		if(additionalStudent.getStudentScore() > this.skbm){
+			this.studentsGraduated.add(additionalStudent);
+		}
 	}
 
 	@Override
