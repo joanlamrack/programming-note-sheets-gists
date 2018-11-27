4999a0ef2ea0421509781818e7c4b21e8d605aff
Joan Lamrack <joanlamrack@gmail.com>
2018-11-27 22:04:03 +0700

    make default output

--- Java/Exercise_2/MoneyInCoins.java
+++ Java/Exercise_2/MoneyInCoins.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class MoneyInCoins{
 	public static void main(String[]args){
 
+	}
+
+	public static int[] doMoneyInCoins(int input){
+		ArrayList<Integer> output = new ArrayList<Integer>();
+
+
+
+		return output.toArray(new Integer[output.size()]);
 	}
 }
