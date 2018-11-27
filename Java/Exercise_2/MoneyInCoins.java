bb540830d40a88d3cd6bc79823d6feb1b4af7a8d
Joan Lamrack <joanlamrack@gmail.com>
2018-11-27 22:10:57 +0700

    money in coins done

--- Java/Exercise_2/MoneyInCoins.java
+++ Java/Exercise_2/MoneyInCoins.java
@@ -1,14 +1,21 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MoneyInCoins{
 	public static void main(String[]args){
-
+		System.out.println( Arrays.toString(doMoneyInCoins(454)) );
 	}
 
-	public static int[] doMoneyInCoins(int input){
+	public static Integer[] doMoneyInCoins(int input){
 		ArrayList<Integer> output = new ArrayList<Integer>();
+		final int[] coins = {10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1};
 
-
+		for(int coin : coins){
+			while(input >= coin){
+				output.add(coin);
+				input-=coin;
+			}
+		}
 
 		return output.toArray(new Integer[output.size()]);
 	}
