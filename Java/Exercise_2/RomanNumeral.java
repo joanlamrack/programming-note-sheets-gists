82ea948fe49b889910a382ae95cadf56c1309a45
Joan Lamrack <joanlamrack@gmail.com>
2018-11-29 06:59:42 +0700

    added crucial variables

--- Java/Exercise_2/RomanNumeral.java
+++ Java/Exercise_2/RomanNumeral.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class RomanNumeral{
 	public static void main(String[]args){
 
@@ -5,6 +7,19 @@ public class RomanNumeral{
 
 	public static String doRomanNumeral(int input){
 		String output = "";
+		int baseTenRomanReference = 1;
+		Map<Integer, String> romanReference = Map.of(
+			1 , "I",
+			5 , "V",
+			10 , "X",
+			50 , "L",
+			100 , "C",
+			500 , "D",
+			1000 , "M" 
+		);
+
+
+
 		
 		return output;
 	}
