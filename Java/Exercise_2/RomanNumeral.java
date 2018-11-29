582a7267259be0b47b272473e7ee6edefe27da92
Joan Lamrack <joanlamrack@gmail.com>
2018-11-29 07:23:01 +0700

    change the proper type

--- Java/Exercise_2/RomanNumeral.java
+++ Java/Exercise_2/RomanNumeral.java
@@ -1,25 +1,37 @@
-import java.util.Map;
+
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
 
 public class RomanNumeral{
 	public static void main(String[]args){
-
+		System.out.println( doRomanNumeral(10));
 	}
 
 	public static String doRomanNumeral(int input){
 		String output = "";
 		int baseTenRomanReference = 1;
-		Map<Integer, String> romanReference = Map.of(
-			1 , "I",
-			5 , "V",
-			10 , "X",
-			50 , "L",
-			100 , "C",
-			500 , "D",
-			1000 , "M" 
-		);
+		LinkedHashMap<Integer, String> romanReference = new  LinkedHashMap<Integer,String>();
 
+		romanReference.put(1000 , "M");
+		romanReference.put(900 , "CM");
+		romanReference.put(500 , "D");
+		romanReference.put(400 , "CD");
+		romanReference.put(100 , "C");
+		romanReference.put(90 , "XC");
+		romanReference.put(50 , "L");
+		romanReference.put(40, "XL");
+		romanReference.put(10 , "X");
+		romanReference.put(9 , "IX");
+		romanReference.put(5 , "V");
+		romanReference.put(1 , "I");
 
-
+		for(int key : romanReference.keySet()){
+			// while(input >= key){
+			// 	output+= romanReference.get(key);
+			// 	input -= key;
+			// }
+			System.out.println(key);
+		}
 		
 		return output;
 	}
