de90b4f1b608e0847c5f9009768dbda88113e3a5
Joan Lamrack <joanlamrack@gmail.com>
2018-12-01 19:32:47 +0700

    working number in words

--- Java/Exercise_2/NumberInWords.java
+++ Java/Exercise_2/NumberInWords.java
@@ -9,9 +9,9 @@ public class NumberInWords {
 		}
 	}
 
-	public static String doNumberInWords(double input){
+	public static String doNumberInWords(double input) {
 		final LinkedHashMap<Double, String> timesTenMapper = new LinkedHashMap<Double, String>();
-		
+
 		timesTenMapper.put(1000000000000.0, "trilyun");
 		timesTenMapper.put(1000000000.0, "milyar");
 		timesTenMapper.put(1000000.0, "juta");
@@ -30,27 +30,35 @@ public class NumberInWords {
 		belowTen.put(4.0, "empat");
 		belowTen.put(3.0, "tiga");
 		belowTen.put(2.0, "dua");
-		belowTen.put(1.0, "satu");
-		belowTen.put(0.0, "");
+		belowTen.put(1.0, "se");
+		belowTen.put(0.0, "nol");
 
-		final String oneSe = "se";
+		final String one = "satu";
 
-		if(input == 0){
-			return "";
-		}
-		else if (input < 10) {
+		if (input == 0) {
+			return belowTen.get(0.0);
+		} else if (input == 1) {
+			return one;
+		} else if (input < 10) {
 			return belowTen.get(input);
-		}
-		else if(input == 10 ){
-			return oneSe+timesTenMapper.get(input);
-		}
-		else if(input > 10 && input < 20) {
+		} else if (input == 10) {
+			return belowTen.get(1.0) + timesTenMapper.get(input);
+		} else if (input > 10 && input < 20) {
 			double teens = input - 10.0;
-			return belowTen.get(teens) +" "+ belowTen.get(10.0);
+			return belowTen.get(teens) + " " + belowTen.get(10.0);
+		} else if (input >= 20) {
+			for (double mapper : timesTenMapper.keySet()) {
+				if (input >= mapper) {
+					double howManytenTimes = Math.floor(input / mapper);
+					double remainder = input % (howManytenTimes * mapper);
+
+					return (howManytenTimes == 1.0 ? belowTen.get(howManytenTimes) : doNumberInWords(howManytenTimes))
+							+ " " + timesTenMapper.get(mapper) + " "
+							+ (remainder == 0 ? "" : doNumberInWords(remainder));
+				}
+			}
 		}
-		else if(input >= 20) {
-			
-		}
+		return "Invalid input";
 
 	}
 }
