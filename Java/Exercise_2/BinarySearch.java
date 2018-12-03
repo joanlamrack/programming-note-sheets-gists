b192f3e717672096737a784d31f384f8e7fe7dfd
Joan Lamrack <joanlamrack@gmail.com>
2018-12-04 06:52:58 +0700

    Added binary Search

---
+++ Java/Exercise_2/BinarySearch.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class BinarySearch {
+	public static void main(String[] args) {
+		int[] input = new int[] {2,6,7,9,14,20};
+		boolean result= doBinarySearch(input, 14);
+
+		System.out.println(result);
+	}
+
+	public static boolean doBinarySearch(int[] input, int toSearch) {
+		// assuming that object already sorted
+		// search for middle array.
+
+		int length = input.length;
+		int middleIndex = 0;
+		int middleElement = 0;
+		if ((length % 2) == 0) {
+			middleIndex = (length / 2) - 1;
+		} else {
+			middleIndex = (length - 1) / 2;
+		}
+
+		middleElement = input[middleIndex];
+		if( Math.abs(middleIndex - length) == 1){
+			return false;
+		}
+		else if(middleElement == toSearch){
+			return true;
+		}
+		else if(middleElement < toSearch){
+			return false || doBinarySearch( Arrays.copyOfRange(input, middleIndex, length-1) , toSearch);
+		}
+		else if(middleElement > toSearch){
+			return false || doBinarySearch( Arrays.copyOfRange(input, 0, middleIndex) , toSearch);
+		}
+
+		return false;
+	}
+}
