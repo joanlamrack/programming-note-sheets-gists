e038029633fc43b13a8783b801437f5b94cb7174
Joan Lamrack <joanlamrack@gmail.com>
2018-12-05 14:33:53 +0700

    working code

--- Java/Exercise_2/LinearSearch.java
+++ Java/Exercise_2/LinearSearch.java
@@ -1,6 +1,7 @@
 public class LinearSearch{
 	public static void main(String[]args){
-
+		int[] input = new int[] {1,6,7,10,9};
+		System.out.println( doLinearSearch(input, 10) );
 	}
 
 	public static boolean doLinearSearch(int[] input, int toSearch){
