60670351f9fbe68cab65a2980b48cc45fe601128
Joan Lamrack <joanlamrack@gmail.com>
2018-12-05 15:09:07 +0700

    added dictionray sort

--- Java/Exercise_2/DictionarySort.java
+++ Java/Exercise_2/DictionarySort.java
@@ -1,10 +1,14 @@
-public class DictionarySort{
-	public static void main(String[]args){
+import java.util.Arrays;
 
+public class DictionarySort {
+	public static void main(String[] args) {
+		String[] input = { "makan", "duduk", "terbang", "tidur" };
+
+		System.out.println(Arrays.toString(doDictionarySort(input)));
 	}
 
-	public static int[] doDictionarySort(){
-		
+	public static String[] doDictionarySort(String[] input) {
+		Arrays.sort(input);
 		return input;
 	}
 }
