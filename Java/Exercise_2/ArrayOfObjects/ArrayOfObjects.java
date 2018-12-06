8f6495fc49135aed731deb1cf20178e6b2582b74
Joan Lamrack <joanlamrack@gmail.com>
2018-12-06 09:05:22 +0700

    added searching

--- Java/Exercise_2/ArrayOfObjects/ArrayOfObjects.java
+++ Java/Exercise_2/ArrayOfObjects/ArrayOfObjects.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class ArrayOfObjects{
     public static void main(String[]args){
+		ArrayList<People> peoples = new ArrayList<People>();
 
+		peoples.add( new People("akabr", People.Gender.MALE, 1));
+		peoples.add( new People("icha", People.Gender.FEMALE, 1));
+
+	}
+
+	public static String searchAndPrint(String name, ArrayList<People> arrayToSearch){
+		for( People onePeople : arrayToSearch){
+			if(onePeople.getName())
+		}
 	}
 }
