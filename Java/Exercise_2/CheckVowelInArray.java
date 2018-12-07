e427412275e5b1987aa43068550cb5b787cffbeb
Joan Lamrack <joanlamrack@gmail.com>
2018-12-07 23:54:41 +0700

    added working program

--- Java/Exercise_2/CheckVowelInArray.java
+++ Java/Exercise_2/CheckVowelInArray.java
@@ -2,49 +2,56 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
-public class CheckVowelInArray{
-	public static void main(String[]args){
-		System.out.println( Arrays.deepToString( generateRandomAlphabetArray(10, 10) ) );
+public class CheckVowelInArray {
+	public static void main(String[] args) {
+		Character[][] arrayTOSearch = generateRandomAlphabetArray(10, 10);
 
-		System.out.println( isVowel('a') );
+		System.out.println(Arrays.deepToString(arrayTOSearch));
+
+		System.out.println(isVowel('a'));
+
+		System.out.println( searchAlphabetBlock(arrayTOSearch) );
 	}
 
-	public static Character[][] generateRandomAlphabetArray(int row, int col){
+	public static Character[][] generateRandomAlphabetArray(int row, int col) {
 		ArrayList<Character[]> output = new ArrayList<Character[]>();
 
-		for(int startRow = 0; startRow < row; startRow++){
+		for (int startRow = 0; startRow < row; startRow++) {
 			ArrayList<Character> rowRandom = new ArrayList<Character>();
-			for(int startCol = 0; startCol < col; startCol++){
-				rowRandom.add( (char)( generateRandomintWithinRange(0, 25)+'a') );
+			for (int startCol = 0; startCol < col; startCol++) {
+				rowRandom.add((char) (generateRandomintWithinRange(0, 25) + 'a'));
 			}
-			output.add( rowRandom.toArray(new Character[rowRandom.size()]) );
+			output.add(rowRandom.toArray(new Character[rowRandom.size()]));
 		}
 
-		return output.toArray( new Character[row][] );
+		return output.toArray(new Character[row][]);
 	}
 
-	public static String searchAlphabetBlock(char[][] arrayToSearchVowel){
-		
-		for(int row = 0 ; row < arrayToSearchVowel.length-1 ; row++){
-			for(int col = 0 ; col < arrayToSearchVowel[0].length-1 ; col++){
-				// if( arrayToSearchVowel[row][col] ) return "";
-			}	
+	public static String searchAlphabetBlock(Character[][] arrayToSearchVowel) {
+
+		for (int row = 0; row < arrayToSearchVowel.length - 1; row++) {
+			for (int col = 0; col < arrayToSearchVowel[0].length - 1; col++) {
+				if (isVowel(arrayToSearchVowel[row][col]) && isVowel(arrayToSearchVowel[row + 1][col])
+						&& isVowel(arrayToSearchVowel[row][col + 1]) && isVowel(arrayToSearchVowel[row + 1][col + 1]))
+					return "FOUND!";
+			}
 		}
 
-		return "";
+		return "NOT FOUND";
 	}
 
-	public static boolean isVowel (char input){
-		char[] vowels = {'a', 'i', 'u', 'e', 'o'}; 
+	public static boolean isVowel(char input) {
+		char[] vowels = { 'a', 'i', 'u', 'e', 'o' };
 
-		for(char vowel : vowels){
-			if(vowel == input) return true;
+		for (char vowel : vowels) {
+			if (vowel == input)
+				return true;
 		}
 
 		return false;
 	}
 
-	public static int generateRandomintWithinRange(int min, int max){
+	public static int generateRandomintWithinRange(int min, int max) {
 		Random rand = new Random();
 		return rand.nextInt(25) + 1;
 	}
