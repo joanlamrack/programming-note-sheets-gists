df8ee7fda0c4b91765ee762956ae6ddf937c2632
Joan Lamrack <joanlamrack@gmail.com>
2018-12-07 23:30:33 +0700

    added random field generator

--- Java/Exercise_2/CheckVowelInArray.java
+++ Java/Exercise_2/CheckVowelInArray.java
@@ -1,13 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class CheckVowelInArray{
 	public static void main(String[]args){
-		System.out.println(  generateRandomintWithinRange(1, 26) );
+		System.out.println( Arrays.deepToString( generateRandomAlphabetArray(10, 10) ) );
 	}
 
-	// public static char[][] generateRandomAlphabetArray(){
+	public static Character[][] generateRandomAlphabetArray(int row, int col){
+		ArrayList<Character[]> output = new ArrayList<Character[]>();
 
-	// }
+		for(int startRow = 0; startRow < row; startRow++){
+			ArrayList<Character> rowRandom = new ArrayList<Character>();
+			for(int startCol = 0; startCol < col; startCol++){
+				rowRandom.add( (char)( generateRandomintWithinRange(0, 25)+'a') );
+			}
+			output.add( rowRandom.toArray(new Character[rowRandom.size()]) );
+		}
+
+		return output.toArray( new Character[row][] );
+	}
 
 	public static String searchAlphabetBlock(char[][] arrayToSearchVowel){
 		return "";
