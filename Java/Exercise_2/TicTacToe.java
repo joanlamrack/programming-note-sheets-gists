c73bd50223e684a3f4788f47173ed563de852990
Joan Lamrack <joanlamrack@gmail.com>
2018-12-09 00:15:06 +0700

    implemented print board

--- Java/Exercise_2/TicTacToe.java
+++ Java/Exercise_2/TicTacToe.java
@@ -1,26 +1,24 @@
+import java.util.Arrays;
 import java.util.Random;
 
-public class TicTacToe{
-	public static void main(String[]args){
-
+public class TicTacToe {
+	public static void main(String[] args) {
+		char[][] generatedBoard = DoTicTacToe();
+		
+		PrintTicTacToe( generatedBoard );
 	}
 
-	public static char[][] DoTicTacToe(){
-		char[][] board = {
-			{'-', '-', '-'},
-			{'-', '-', '-'},
-			{'-', '-', '-'}
-		};
+	public static char[][] DoTicTacToe() {
+		char[][] board = { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } };
 
 		int Xcount = 0;
 		int Ocount = 0;
 
-		for(int rowIndex = 0 ; rowIndex < board.length ; rowIndex++){
-			for(int colIndex = 0 ; colIndex < board[0].length ; colIndex++){
-				if(isX() && ((Xcount == 5 && Ocount == 4 ) || (Ocount == 5 && Xcount == 5))){
+		for (int rowIndex = 0; rowIndex < board.length; rowIndex++) {
+			for (int colIndex = 0; colIndex < board[0].length; colIndex++) {
+				if (isX() && ((Xcount == 5 && Ocount == 4) || (Ocount == 5 && Xcount == 5))) {
 					board[rowIndex][colIndex] = 'X';
-				}
-				else{
+				} else {
 					board[rowIndex][colIndex] = 'O';
 				}
 			}
@@ -29,8 +27,14 @@ public class TicTacToe{
 		return board;
 	}
 
-	public static boolean isX(){
+	public static void PrintTicTacToe(char[][] board) {
+		for (char[] row : board) {
+			System.out.println(Arrays.toString(row));
+		}
+	}
+
+	public static boolean isX() {
 		Random rand = new Random();
-		return rand.nextInt(2);
+		return  1 == rand.nextInt(2);
 	}
 }
