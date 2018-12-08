523ced3ca806c69c888ef5549bd8cf99881f5966
Joan Lamrack <joanlamrack@gmail.com>
2018-12-08 21:06:14 +0700

    refactor main code

--- Java/Exercise_2/SnakeLadder.java
+++ Java/Exercise_2/SnakeLadder.java
@@ -4,17 +4,18 @@ import java.util.Collections;
 
 public class SnakeLadder {
 	public static void main(String[] args) {
-		System.out.println( Arrays.deepToString( doSnakeLadder(10) ) );
+		Integer[][] generatedSnakeLadder = doSnakeLadder(10);
+		PrintArray(generatedSnakeLadder);
 	}
 
 	public static Integer[][] doSnakeLadder(int widthHeightLadder) {
 		ArrayList<Integer[]> output = new ArrayList<Integer[]>();
 		ArrayList<Integer> row = new ArrayList<Integer>();
 		for (int start = 1; start < (widthHeightLadder * widthHeightLadder); start++) {
-			
+
 			row.add(start);
-			if(start % widthHeightLadder == 0){
-				output.add( row.toArray( new Integer[row.size()]) );
+			if (start % widthHeightLadder == 0) {
+				output.add(row.toArray(new Integer[row.size()]));
 				row.clear();
 			}
 		}
@@ -27,9 +28,9 @@ public class SnakeLadder {
 		return input;
 	}
 
-	public static void PrintArray(Integer[][] inputToPrint){
-		for(Integer[] row : inputToPrint){
-			System.out.println( Arrays.toString( row ));
+	public static void PrintArray(Integer[][] inputToPrint) {
+		for (Integer[] row : inputToPrint) {
+			System.out.println(Arrays.toString(row));
 		}
 	}
 }
