58c83c3343d0e8456be5896e72fcbcc3ccf2482b
Joan Lamrack <joanlamrack@gmail.com>
2018-12-08 23:53:18 +0700

    create initial file

---
+++ Java/Exercise_2/TicTacToe.java
@@ -0,0 +1,15 @@
+public class TicTacToe{
+	public static void main(String[]args){
+
+	}
+
+	public static char[][] DoTicTacToe(){
+		char[][] board = {
+			{'-', '-', '-'},
+			{'-', '-', '-'},
+			{'-', '-', '-'}
+		};
+
+		return board;
+	}
+}
