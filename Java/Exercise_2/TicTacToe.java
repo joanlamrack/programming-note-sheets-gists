d8cde550159a220464118616b8904ee8e50c5e94
Joan Lamrack <joanlamrack@gmail.com>
2018-12-09 00:11:05 +0700

    implement tic tac toe board generator

--- Java/Exercise_2/TicTacToe.java
+++ Java/Exercise_2/TicTacToe.java
@@ -12,6 +12,20 @@ public class TicTacToe{
 			{'-', '-', '-'}
 		};
 
+		int Xcount = 0;
+		int Ocount = 0;
+
+		for(int rowIndex = 0 ; rowIndex < board.length ; rowIndex++){
+			for(int colIndex = 0 ; colIndex < board[0].length ; colIndex++){
+				if(isX() && ((Xcount == 5 && Ocount == 4 ) || (Ocount == 5 && Xcount == 5))){
+					board[rowIndex][colIndex] = 'X';
+				}
+				else{
+					board[rowIndex][colIndex] = 'O';
+				}
+			}
+		}
+
 		return board;
 	}
 
