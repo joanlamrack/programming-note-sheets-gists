7bb1056d7a30ad3a2d088baf7e9786a98e011177
Joan Lamrack <joanlamrack@gmail.com>
2018-12-08 21:03:11 +0700

    added print function

--- Java/Exercise_2/SnakeLadder.java
+++ Java/Exercise_2/SnakeLadder.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class SnakeLadder {
 	public static void main(String[] args) {
-
+		System.out.println( Arrays.deepToString( doSnakeLadder(10) ) );
 	}
 
 	public static Integer[][] doSnakeLadder(int widthHeightLadder) {
 		ArrayList<Integer[]> output = new ArrayList<Integer[]>();
-
+		ArrayList<Integer> row = new ArrayList<Integer>();
 		for (int start = 1; start < (widthHeightLadder * widthHeightLadder); start++) {
-			ArrayList<Integer> row = new ArrayList<Integer>();
+			
+			row.add(start);
 			if(start % widthHeightLadder == 0){
+				output.add( row.toArray( new Integer[row.size()]) );
 				row.clear();
 			}
 		}
@@ -19,8 +22,14 @@ public class SnakeLadder {
 		return output.toArray(new Integer[output.size()][]);
 	}
 
-	public static Arraylist<?> reverseArrayList(Arraylist<?> input) {
-		Collection.reverse(input);
+	public static ArrayList<?> reverseArrayList(ArrayList<?> input) {
+		Collections.reverse(input);
 		return input;
+	}
+
+	public static void PrintArray(Integer[][] inputToPrint){
+		for(Integer[] row : inputToPrint){
+			System.out.println( Arrays.toString( row ));
+		}
 	}
 }
