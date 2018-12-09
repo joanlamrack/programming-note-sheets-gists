f979c7608110298c0d748e312d4ca1d5859aa3d4
Joan Lamrack <joanlamrack@gmail.com>
2018-12-09 17:54:43 +0700

    First Attempt Clear Screen

--- Java/Exercise_2/JsRacer.java
+++ Java/Exercise_2/JsRacer.java
@@ -1,9 +1,27 @@
 public class JsRacer{
 	public static void main(String[]args){
-
+		System.out.println("HAHHAHAH");
+		ClearScreen();
 	}
 
 	public static void ClearScreen(){
-		
+		try
+		{
+			final String os = System.getProperty("os.name");
+	
+			if (os.contains("Windows"))
+			{
+				Runtime.getRuntime().exec("cls");
+			}
+			else
+			{
+				Runtime.getRuntime().exec("clear");
+			}
+		}
+		catch (final Exception e)
+		{
+			e.printStackTrace();
+			System.out.println("Error Happened"); 
+		}
 	}
 }
