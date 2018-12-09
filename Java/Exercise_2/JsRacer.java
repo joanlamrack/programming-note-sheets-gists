c10f87f30d94fd150b55221424f3b3394000cc1a
Joan Lamrack <joanlamrack@gmail.com>
2018-12-09 18:40:54 +0700

    implement generate Alphabet from integer

--- Java/Exercise_2/JsRacer.java
+++ Java/Exercise_2/JsRacer.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.HashMap;
 
 public class JsRacer{
 	public static void main(String[]args){
@@ -17,4 +18,13 @@ public class JsRacer{
 	public static void ClearScreen() throws IOException, InterruptedException {
 		new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 	}
+
+	public static Character generateAlphabetFromInteger(int integerInput){
+		return (char) (integerInput - 1) + 'a';
+	}
+
+	// public static HashMap<Character,Integer> playerList(int playerCount){
+		
+	// 	return
+	// }
 }
