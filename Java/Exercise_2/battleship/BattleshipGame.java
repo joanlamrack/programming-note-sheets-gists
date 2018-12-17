5026ec0098434bfe1c0330bf8b45a993a9aaaad9
Joan Lamrack <joanlamrack@gmail.com>
2018-12-17 09:52:45 +0700

    make main BattleShipGameBoard

---
+++ Java/Exercise_2/battleship/BattleshipGame.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class BattleshipGame {
+	HashMap<Character, Integer> enemiesData = null;
+	int boardLength = 0;
+	int boardLastIndex = boardLength - 1;
+
+	public BattleshipGame(HashMap<Character, Integer> enemiesData, int boardLength) {
+		
+	}
+}
