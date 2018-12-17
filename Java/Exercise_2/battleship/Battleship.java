792ddd497bf659570b35d837731a24fb3d687cb1
Joan Lamrack <joanlamrack@gmail.com>
2018-12-17 09:57:36 +0700

    added stub for main game

---
+++ Java/Exercise_2/battleship/Battleship.java
@@ -0,0 +1,11 @@
+public class Battleship{
+	public static void main(String[]args){
+		HashMap<Character, Integer> enemiesData = new HashMap<Character, Integer>();
+		enemiesData.put('b', 4);
+		enemiesData.put('c', 5);
+		enemiesData.put('d', 2);
+		char[][] board = generateBoardWithEnemies(enemiesData);
+
+		printBoard(board);
+	}
+}
