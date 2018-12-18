ff83dd30fa536aff5154c888188dffbf7cc37a91
Joan Lamrack <joanlamrack@gmail.com>
2018-12-18 08:59:34 +0700

    deleted unncessary clear screen

--- Java/Exercise_2/battleship/BattleshipBoard.java
+++ Java/Exercise_2/battleship/BattleshipBoard.java
@@ -1,34 +1,8 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
-import java.io.IOException;
-import java.lang.InterruptedException;
 
 public class BattleshipBoard {
-
-	public static void doSleep() {
-		sleep(2000);
-	}
-
-	public static void sleep(int milliseconds) {
-		try {
-			Thread.sleep(milliseconds);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void doClearScreen() {
-		try {
-			ClearScreen();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void ClearScreen() throws IOException, InterruptedException {
-		new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-	}
 
 	public static char[][] generatePlainBoard(int boardLength) {
 		char[][] board = new char[boardLength][boardLength];
