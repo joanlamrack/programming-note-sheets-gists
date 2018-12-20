a359642a98bfff4579f8c63569a4032aebd3de42
Joan Lamrack <joanlamrack@gmail.com>
2018-12-20 08:59:29 +0700

    added reading file logic

--- Java/Exercise_2/sudoku/sudokusolver.java
+++ Java/Exercise_2/sudoku/sudokusolver.java
@@ -2,6 +2,10 @@ import java.nio.file.Files;
 
 public class sudokusolver {
 	public static void main(String[] args) {
+		
+	}
 
+	public static String doReadFile(String fileName){
+		return new String(Files.readAllBytes(Paths.get(fileName)))
 	}
 }
