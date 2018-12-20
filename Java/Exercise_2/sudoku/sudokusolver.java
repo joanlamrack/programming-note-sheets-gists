580af3d0c28a27d7fcadd811bbcd05b58b269b51
Joan Lamrack <joanlamrack@gmail.com>
2018-12-20 09:27:22 +0700

    migrate to outer library

--- Java/Exercise_2/sudoku/sudokusolver.java
+++ Java/Exercise_2/sudoku/sudokusolver.java
@@ -2,9 +2,7 @@ import java.util.Arrays;
 
 public class sudokusolver {
 	public static void main(String[] args) {
-		String fromFile = doReadFile("set01-unsolved.txt");
+		String fromFile = FileReader.doReadFile("set01-unsolved.txt");
 		System.out.println(Arrays.toString( FileReader.separateByEnter(fromFile) ));
 	}
-
-
 }
