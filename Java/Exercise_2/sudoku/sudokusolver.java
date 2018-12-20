3ed9fd73365911d48483297af807a3595ad32068
Joan Lamrack <joanlamrack@gmail.com>
2018-12-20 09:26:41 +0700

    move the file reading into  separate file

--- Java/Exercise_2/sudoku/sudokusolver.java
+++ Java/Exercise_2/sudoku/sudokusolver.java
@@ -1,23 +1,10 @@
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class sudokusolver {
 	public static void main(String[] args) {
 		String fromFile = doReadFile("set01-unsolved.txt");
-		System.out.println(Arrays.toString( separateByEnter(fromFile) ));
+		System.out.println(Arrays.toString( FileReader.separateByEnter(fromFile) ));
 	}
 
-	public static String doReadFile(String fileName) {
-		try {
-			return new String(Files.readAllBytes(Paths.get(fileName)));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "";
-	}
 
-	public static String[] separateByEnter(String input){
-		return input.split("\\r?\\n", -1);
-	}
 }
