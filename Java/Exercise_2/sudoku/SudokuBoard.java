8ed05132e7a929440def1138820cf68bb242194e
Joan Lamrack <joanlamrack@gmail.com>
2018-12-22 14:04:29 +0700

    added print board logic

--- Java/Exercise_2/sudoku/SudokuBoard.java
+++ Java/Exercise_2/sudoku/SudokuBoard.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SudokuBoard{
 	private int[][] board = new int[9][9];
 	private int boardLength = 9;
@@ -7,14 +9,18 @@ public class SudokuBoard{
 	}
 
 	private int[][] fillSudokuBoard(String input){
-		for(int start = 0; start < 81 ; start=start+9){
-			for(int row = start; row< start+9; row++){
-				input.substring(, endIndex)
-			}
-		}
+		
 	}
 
+	// private 
+
+	// private void placeNumberOnBoard(int rowIndex, int colIndex, int input){
+
+	// }
+
 	public void printBoard(){
-		
+		for(int[] row: this.board){
+			System.out.println( Arrays.toString(row) );
+		}
 	}
 }
