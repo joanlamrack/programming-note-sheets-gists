e20b29885ed77c9848840949f332fbfaf4d97c37
Joan Lamrack <joanlamrack@gmail.com>
2018-12-22 16:54:07 +0700

    tried

--- Java/Exercise_2/sudoku/sudokusolver.java
+++ Java/Exercise_2/sudoku/sudokusolver.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 public class sudokusolver {
 	public static void main(String[] args) {
 		String fromFile = FileReader.doReadFile("set01-unsolved.txt");
-		System.out.println(Arrays.toString( FileReader.separateByEnter(fromFile) ));
+		String[] inputs = FileReader.separateByEnter(fromFile);
+
+		SudokuBoard board = new SudokuBoard(inputs[0]);
+		board.printBoard();
 	}
 }
