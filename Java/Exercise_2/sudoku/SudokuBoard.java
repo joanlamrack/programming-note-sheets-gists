5ee05447b87bd67d1876c04b88278df47ef82318
Joan Lamrack <joanlamrack@gmail.com>
2018-12-22 13:58:36 +0700

    add board length

--- Java/Exercise_2/sudoku/SudokuBoard.java
+++ Java/Exercise_2/sudoku/SudokuBoard.java
@@ -1,12 +1,17 @@
 public class SudokuBoard{
-	int[][] board = new int[9][9];
+	private int[][] board = new int[9][9];
+	private int boardLength = 9;
 
 	public SudokuBoard(String sudokuvalue){
 
 	}
 
 	private int[][] fillSudokuBoard(String input){
-		
+		for(int start = 0; start < 81 ; start=start+9){
+			for(int row = start; row< start+9; row++){
+				input.substring(, endIndex)
+			}
+		}
 	}
 
 	public void printBoard(){
