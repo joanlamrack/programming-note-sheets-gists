4e8dde0a9e8d03241b426cc4072652c28315d5e0
Joan Lamrack <joanlamrack@gmail.com>
2018-12-25 21:35:04 +0700

    fixed classed naming

--- Java/Exercise_2/boggle/PlayBoogle.java
+++ Java/Exercise_2/boggle/PlayBoogle.java
@@ -1,7 +1,6 @@
 public class PlayBoogle{
 	public static void main(String[] args){
-		BoogleBoard oneGameSession = new BoggleBoard();
-
+		BoggleBoard oneGameSession = new BoggleBoard();
 		oneGameSession.shake(4);
 		oneGameSession.printBoard();
 	}
