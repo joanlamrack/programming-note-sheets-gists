dafd18089b061ed9ea4c8ca7b33c19d27e4e0e62
Joan Lamrack <joanlamrack@gmail.com>
2018-12-25 19:03:53 +0700

    implement file reading logic

--- Java/Exercise_2/boggle/data.java
+++ Java/Exercise_2/boggle/data.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+
 public class data {
+	
 	public static String[] getData(){
 		String readFromFile = FileReader.doReadFile("data.txt");
 		String[] outputArray = FileReader.separateByEnter(readFromFile);
 		return outputArray;
 	}
+
 }
