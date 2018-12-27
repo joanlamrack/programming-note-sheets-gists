a84c90d0be0ca7b687aba5da2f6b0f5378ac6cb3
Joan Lamrack <joanlamrack@gmail.com>
2018-12-27 13:49:48 +0700

    added row translation

--- Java/Exercise_2/boggle/Direction.java
+++ Java/Exercise_2/boggle/Direction.java
@@ -13,7 +13,7 @@ public class BoogleDirection{
 		UP_RIGHT
 	}
 
-	private static Map<String, String> OppositeDirection = Map.of(
+	private static Map<String, String> oppositeDirection = Map.of(
 		DirectionPointer.DOWN.toString(), DirectionPointer.UP.toString(),
 		DirectionPointer.UP.toString(), DirectionPointer.DOWN.toString,
 		DirectionPointer.LEFT.toString(), DirectionPointer.RIGHT.toString(),
@@ -22,6 +22,17 @@ public class BoogleDirection{
 		DirectionPointer.DOWN_LEFT.toString(), DirectionPointer.UP_RIGHT.toString,
 		DirectionPointer.UP_RIGHT.toString, DirectionPointer.DOWN_LEFT.toString(),
 		DirectionPointer.DOWN_RIGHT, DirectionPointer.UP_LEFT.toString()
+	);
+
+	private static Map<String, Integer> rowTranslation = Map.of(
+		DirectionPointer.RIGHT.toString(), 0,
+		DirectionPointer.LEFT.toString(), 0,
+		DirectionPointer.UP.toString(), -1,
+		DirectionPointer.DOWN.toString(), 1,
+		DirectionPointer.UP_LEFT.toString(), -1,
+		DirectionPointer.UP_RIGHT.toString(), 1,
+		DirectionPointer.DOWN_LEFT.toString(), 1,
+		DirectionPointer.DOWN_RIGHT.toString(), 1
 	);
 
 }
