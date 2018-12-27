2b5ac1cbed51c01dee9ade25f3b8231a7022887a
Joan Lamrack <joanlamrack@gmail.com>
2018-12-27 12:25:19 +0700

    added enum

---
+++ Java/Exercise_2/boggle/Direction.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class BoogleDirection{
+	private static enum DirectionPointer {
+		RIGHT,
+		DOWN_RIGHT,
+		DOWN,
+		DOWN_LEFT,
+		LEFT,
+		UP_LEFT,
+		UP,
+		UP_RIGHT
+	}
+
+}
