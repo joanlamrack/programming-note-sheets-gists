e37112144d666ce96403c2c1ddb4c63b3fd1002e
Joan Lamrack <joanlamrack@gmail.com>
2018-12-27 13:54:25 +0700

    adde col translation

--- Java/Exercise_2/boggle/Direction.java
+++ Java/Exercise_2/boggle/Direction.java
@@ -20,8 +20,8 @@ public class BoogleDirection{
 		DirectionPointer.RIGHT.toString(), DirectionPointer.LEFT.toString(),
 		DirectionPointer.UP_LEFT.toString(), DirectionPointer.DOWN_RIGHT.toString(),
 		DirectionPointer.DOWN_LEFT.toString(), DirectionPointer.UP_RIGHT.toString,
-		DirectionPointer.UP_RIGHT.toString, DirectionPointer.DOWN_LEFT.toString(),
-		DirectionPointer.DOWN_RIGHT, DirectionPointer.UP_LEFT.toString()
+		DirectionPointer.UP_RIGHT.toString(), DirectionPointer.DOWN_LEFT.toString(),
+		DirectionPointer.DOWN_RIGHT.toString(), DirectionPointer.UP_LEFT.toString()
 	);
 
 	private static Map<String, Integer> rowTranslation = Map.of(
@@ -35,4 +35,14 @@ public class BoogleDirection{
 		DirectionPointer.DOWN_RIGHT.toString(), 1
 	);
 
+	private static Map<String, Integer> colTranslation = Map.of(
+		DirectionPointer.RIGHT.toString(), 1,
+		DirectionPointer.LEFT.toString(), -1,
+		DirectionPointer.UP.toString(), 0,
+		DirectionPointer.DOWN.toString(), 0,
+		DirectionPointer.UP_LEFT.toString(), -1,
+		DirectionPointer.UP_RIGHT.toString(), 1,
+		DirectionPointer.DOWN_LEFT.toString(), -1,
+		DirectionPointer.DOWN_RIGHT.toString(), 1
+	);
 }
