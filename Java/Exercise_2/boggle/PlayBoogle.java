2fd25bf4a8533291da0ba835a16c1de77f9f9a24
Joan Lamrack <joanlamrack@gmail.com>
2018-12-28 15:42:18 +0700

    removed unused librarby

--- Java/Exercise_2/boggle/PlayBoogle.java
+++ Java/Exercise_2/boggle/PlayBoogle.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class PlayBoogle{
 	public static void main(String[] args){
 		BoggleBoard oneGameSession = new BoggleBoard();
 		oneGameSession.shake(5);
 		oneGameSession.printBoard();
+		System.out.println(  Arrays.toString(BoogleDirection.getDirections()) );
 	}
 }
